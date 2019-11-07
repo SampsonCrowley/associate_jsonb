9abe0dd9724a442f45f525377594faead6ac0439
Adrien Montfort <adrien@sorare.com>
2019-11-07 14:38:02 +0100

    Fix has_one and has_many associations

--- lib/activerecord/jsonb/associations/association_scope.rb
+++ lib/activerecord/jsonb/associations/association_scope.rb
@@ -13,21 +13,23 @@ module ActiveRecord
           value = transform_value(owner[join_keys.foreign_key])
 
           if reflection.options.key?(:foreign_store)
-            apply_jsonb_scope(
+            apply_jsonb_equality(
               scope,
-              jsonb_equality(table, reflection.options[:foreign_store], key),
-              key, value
+              table,
+              reflection.options[:foreign_store],
+              key,
+              value
             )
-          elsif reflection && reflection.options.key?(:store)
+          elsif reflection.options.key?(:store)
             return super if reflection.belongs_to?
             pluralized_key = key.pluralize
 
-            apply_jsonb_scope(
+            apply_jsonb_containment(
               scope,
-              jsonb_containment(
-                table, reflection.options[:store], pluralized_key
-              ),
-              pluralized_key, value
+              table,
+              reflection.options[:store],
+              pluralized_key,
+              value
             )
           else
             super
@@ -35,24 +37,32 @@ module ActiveRecord
         end
         # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
-        def apply_jsonb_scope(scope, predicate, key, value)
-          scope.where!(predicate).tap do |arel_scope|
-            arel_scope.where_clause.binds << Relation::QueryAttribute.new(
-              key.to_s, value, ActiveModel::Type::String.new
+        def apply_jsonb_equality(scope, table, jsonb_column, key, value)
+          scope.where!(
+            Arel::Nodes::JSONBDashDoubleArrow.new(
+              table, table[jsonb_column], key
+            ).eq(
+              Arel::Nodes::BindParam.new(
+                Relation::QueryAttribute.new(
+                  key.to_s, value, ActiveModel::Type::String.new
+                )
+              )
             )
-          end
+          )
         end
 
-        def jsonb_equality(table, jsonb_column, key)
-          Arel::Nodes::JSONBDashDoubleArrow.new(
-            table, table[jsonb_column], key
-          ).eq(Arel::Nodes::BindParam.new)
-        end
-
-        def jsonb_containment(table, jsonb_column, key)
-          Arel::Nodes::JSONBHashArrow.new(
-            table, table[jsonb_column], key
-          ).contains(Arel::Nodes::BindParam.new)
+        def apply_jsonb_containment(scope, table, jsonb_column, key, value)
+          scope.where!(
+            Arel::Nodes::JSONBHashArrow.new(
+              table, table[jsonb_column], key
+            ).contains(
+              Arel::Nodes::BindParam.new(
+                Relation::QueryAttribute.new(
+                  key.to_s, value, ActiveModel::Type::String.new
+                )
+              )
+            )
+          )
         end
       end
     end
